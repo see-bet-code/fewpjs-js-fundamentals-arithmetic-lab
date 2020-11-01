4ce598812cb16ca746ccd7a776691f86f6fbf68c
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-01 11:46:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * Infinity) + 1;
+const mod = 24 % 5
+const max = Math.max(10, 20)
